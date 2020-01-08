9cb5b1f4348d2254455b0815287e62065b9233a3
Roux13 <u.nehodov@gmail.com>
2020-01-08 23:20:49 +0300

    4.2 Максимум из двух чисел.[#220023] Убрана проверка на ноль.

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -4,11 +4,9 @@ public class Factorial {
 
     public int calc(int n) {
         int result = 1;
-        if (n != 0) {
             for (int i = 1; i <= n; i++) {
                 result *= i;
             }
-        }
         return result;
     }
 
