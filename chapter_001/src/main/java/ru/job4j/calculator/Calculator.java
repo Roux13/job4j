1baacd44df11db18e24caf8adc4ebe0add490d3d
Roux13 <u.nehodov@gmail.com>
2020-01-09 21:37:57 +0300

    5.8. JavaDoc. Code convention[#220096]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,6 +2,19 @@ package ru.job4j.calculator;
 
 import java.util.Locale;
 
+/**
+ * Class Calculator for calculating arithmetic operations:
+ * 1.add - addition.
+ * 2.div - division.
+ * 3.multiply - multiplication.
+ * 4.subtrack - subtraction.
+ *
+ * @author Yuri Nehodov
+ * @since 5.01.2020
+ * @version 1.0
+ */
+
+
 public class Calculator{
 
     public static void add(double first, double second) {
@@ -20,6 +33,12 @@ public class Calculator{
         System.out.printf(Locale.ENGLISH, "%,.1f - %,.1f = %,.1f%n", first, second, first - second);
     }
 
+    /**
+     * Method main for testing class Calculator.
+     * It is testing Calculator's methods with fixed parameters.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculator task.
+ *
+ * @author Yuri Nehodov (u.nehodov@gmail.com)
+ * @version $Id$
+ * @since 5.01.2020
+ */
+package ru.job4j.calculator;
