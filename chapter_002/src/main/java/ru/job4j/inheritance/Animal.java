33fb300b021659660fa8dd85c65192bd4bda356f
Roux13 <u.nehodov@gmail.com>
2020-01-18 20:30:55 +0300

    Часть 002. ООП / 2. Наследование / 2. Перегрузка конструктора.[#220195]

--- chapter_002/src/main/java/ru/job4j/inheritance/Animal.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/Animal.java
@@ -2,7 +2,13 @@ package ru.job4j.inheritance;
 
 public class Animal {
 
+    private String name;
+
     public Animal() {
         System.out.println("It's Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
     }
 }
