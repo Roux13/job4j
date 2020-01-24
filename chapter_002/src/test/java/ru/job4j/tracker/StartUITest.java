fce0ee5d0304313c9c2ba0d8ce6628512199a4ad
Roux13 <u.nehodov@gmail.com>
2020-01-24 06:38:13 +0300

    Часть 002. ООП / 4. Полиморфизм / 5.1. Тестирование. Подготовка данных.[#220161]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -28,4 +28,30 @@ public class StartUITest {
         Item expected = new Item(message);
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        String newMessage = "New Item";
+        String replacedMessage = "Replaced Item";
+        Tracker tracker = new Tracker();
+        Item item = new Item(newMessage);
+        tracker.add(item);
+        String[] answers = {item.getId(), replacedMessage};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is(replacedMessage));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        String newMessage = "New Item";
+        Tracker tracker = new Tracker();
+        Item item = new Item(newMessage);
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        Item expected = null;
+        assertThat(deleted, is(expected));
+    }
 }
