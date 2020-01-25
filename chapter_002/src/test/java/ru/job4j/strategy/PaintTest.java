cfab234801040860b75e3f64cfea2ae74d04eb39
Roux13 <u.nehodov@gmail.com>
2020-01-25 17:37:11 +0300

    Часть 002. ООП / 4. Полиморфизм / 9.1 Рефакторинг теста @Before @After[#220150]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -7,11 +9,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+
+    private final PrintStream stdOut = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(stdOut);
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdOut = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         String expected = new StringBuilder().append("*  *  *  *  *  *  *").append("\n")
                 .append("*                 *").append("\n")
