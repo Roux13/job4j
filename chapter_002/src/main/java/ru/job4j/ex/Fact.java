2c14fcbd6109189b14f5dfc0fdd087529cb357d4
Roux13 <u.nehodov@gmail.com>
2020-01-25 21:01:07 +0300

    Часть 002. ООП / 5. Исключения / 0.2. Кидаем исключение - throw new RuntimeException[#220173]

---
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(0));
+    }
+
+    public int calc(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Argument n should be more or equals by 1");
+        }
+        int rsl = 1;
+        for (int index = 1; index != n; index++) {
+            rsl += index;
+        }
+        return rsl;
+    }
+}
