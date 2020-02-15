18fd83d45cb6f692a4577c42d66f303c0fade793
Roux13 <u.nehodov@gmail.com>
2020-02-15 18:25:19 +0300

    Часть 004. FP, Lambda, Stream API / 1. Labmda / 2. Stream API / 2. Список адресов[#219996]

---
+++ chapter_004/src/test/java/ru/job4j/stream/tourist/ProfilesTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream.tourist;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void collect() {
+        Address address1 = new Address("Moscow", "Tverskaya", 15, 7);
+        Address address2 = new Address("London", "Baker st.", 46, 1);
+        Address address3 = new Address("New York", "Park avenue", 111, 11);
+        Profile client1 = new Profile(address1);
+        Profile client2 = new Profile(address2);
+        Profile client3 = new Profile(address3);
+        List<Profile> in = List.of(client1, client2, client3);
+        List<Address> expect = List.of(address1, address2, address3);
+        List<Address> result = new Profiles().collect(in);
+        assertThat(result, is(expect));
+    }
+
+}
