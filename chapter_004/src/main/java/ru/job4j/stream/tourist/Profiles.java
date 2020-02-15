7cba11440e9b13ad737353faf3a8126986137aa4
Roux13 <u.nehodov@gmail.com>
2020-02-15 18:41:19 +0300

    Часть 004. FP, Lambda, Stream API / 1. Labmda / 2. Stream API / 3. Уникальность элементов и сортировка.[#219997]

--- chapter_004/src/main/java/ru/job4j/stream/tourist/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/tourist/Profiles.java
@@ -6,7 +6,11 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public List<Address> collect(List<Profile> profiles) {
-       return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+       return profiles.stream()
+               .map(Profile::getAddress)
+               .sorted(new AddressComparator())
+               .distinct()
+               .collect(Collectors.toList());
     }
 
 }
--- chapter_004/src/test/java/ru/job4j/stream/tourist/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/tourist/ProfilesTest.java
@@ -18,7 +18,22 @@ public class ProfilesTest {
         Profile client2 = new Profile(address2);
         Profile client3 = new Profile(address3);
         List<Profile> in = List.of(client1, client2, client3);
-        List<Address> expect = List.of(address1, address2, address3);
+        List<Address> expect = List.of(address2, address1, address3);
+        List<Address> result = new Profiles().collect(in);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void collectWithDuplicates() {
+        Address address1 = new Address("Moscow", "Tverskaya", 15, 7);
+        Address address2 = new Address("London", "Baker st.", 46, 1);
+        Address address3 = new Address("New York", "Park avenue", 111, 11);
+        Profile client1 = new Profile(address1);
+        Profile client2 = new Profile(address2);
+        Profile client22 = new Profile(address2);
+        Profile client3 = new Profile(address3);
+        List<Profile> in = List.of(client1, client2, client22, client3);
+        List<Address> expect = List.of(address2, address1, address3);
         List<Address> result = new Profiles().collect(in);
         assertThat(result, is(expect));
     }
